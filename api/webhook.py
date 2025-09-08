8390070f1dbe59b154413926051cc27b41b9382c
wilawan047 <computersci65@gmail.com>
2025-09-08 19:25:55 +0700

    แกไข webhook

--- api/webhook.py
+++ api/webhook.py
@@ -865,8 +865,8 @@ def handle_sticker(event):
         ),
     )
 
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
 
 
